872f93f01119375b6fc3ace5044860e928192df8
Jeong <jyunj3015@gmail.com>
2024-09-22 07:46:19 +0900

    Add : seo 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: "https://underwater-path.vercel.app/",
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: "daily",
+    priority: 1,
+    exclude: [],
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: "*",
+          allow: "/",
+          disallow: [],
+        },
+      ],
+    },
+  };
