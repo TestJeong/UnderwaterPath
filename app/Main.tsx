358e48816c78249ac1d485a07a49db7b8638a758
Jeong <jyunj3015@gmail.com>
2024-10-30 16:20:37 +0900

    modify : 스타일 수정

--- app/Main.tsx
+++ app/Main.tsx
@@ -8,7 +8,7 @@ const MAX_DISPLAY = 8
 export default function Home({ posts }) {
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="divide-y divide-gray-200 px-2 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             글
@@ -34,7 +34,7 @@ export default function Home({ posts }) {
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight mobile:text-xl">
                             <Link
                               href={`/blog/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
@@ -48,7 +48,7 @@ export default function Home({ posts }) {
                             ))}
                           </div>
                         </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400 mobile:text-sm">
                           {summary}
                         </div>
                       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      screens: {
+        mobile: { max: '400px' },
+        // => @media (min-width: 640px) { ... }
+      },
       lineHeight: {
         11: '2.75rem',
         12: '3rem',
@@ -23,6 +27,10 @@ module.exports = {
       },
       fontFamily: {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
+      },
+      fontSize: {
+        'h1-mobile': '0.5rem', // h1 폰트 크기 설정 예시
+        'h2-mobile': '1rem', // h2 폰트 크기 설정 예시
       },
       colors: {
         primary: colors.green,
@@ -58,9 +66,18 @@ module.exports = {
                 color: `${theme('colors.primary.400')}`,
               },
               code: { color: theme('colors.primary.400') },
+              whiteSpace: 'pre-wrap', // 자동 줄바꿈 적용
+              wordBreak: 'break-word', // 긴 단어 강제 줄바꿈
             },
             'h1,h2,h3,h4,h5,h6': {
               color: theme('colors.gray.100'),
+              '@screen mobile': {
+                fontSize: (theme) => ({
+                  h1: theme('fontSize.h1-mobile'),
+                  h2: theme('fontSize.h2-mobile'),
+                  // 추가적인 h3, h4 등 필요한 요소 설정
+                }),
+              },
             },
           },
         },
