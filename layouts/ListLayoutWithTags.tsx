c640a283ff3242ba2546a8602577a083300b6450
Jeong <jyunj3015@gmail.com>
2025-01-05 21:24:14 +0900

    Modify : all posts font size 수정

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -134,7 +134,7 @@ export default function ListLayoutWithTags({
                       </dl>
                       <div className="space-y-3">
                         <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight mobile:text-xl">
                             <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
                               {title}
                             </Link>
@@ -143,7 +143,7 @@ export default function ListLayoutWithTags({
                             {tags?.map((tag) => <Tag key={tag} text={tag} />)}
                           </div>
                         </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                        <div className=" prose max-w-none text-gray-500 dark:text-gray-400 mobile:text-sm">
                           {summary}
                         </div>
                       </div>
