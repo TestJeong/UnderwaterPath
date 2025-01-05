6c382436e35d9b4a1d5ccf3ca7ef0e26fb5ec70a
Jeong <jyunj3015@gmail.com>
2025-01-05 17:49:04 +0900

    Modify : SEO 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: "https://underwater-path.vercel.app",
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-    changefreq: "daily",
-    priority: 1,
-    exclude: [],
-  };
+  siteUrl: 'https://underwater-path.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 1,
+  exclude: [],
+  additionalPaths: async (config) => [
+    {
+      loc: '/about',
+      changefreq: 'daily',
+      priority: 1,
+    },
+  ],
+}
